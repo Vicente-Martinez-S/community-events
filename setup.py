994f7d43d76c249d03f33670f222253809a0a911
Vicente-Martinez-S <vicente.martinez02@alumnos.uach.cl>
2023-10-21 12:59:23 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,4 +3,8 @@ from setuptools import setup, find_packages
 with open("requirements.txt", "r") as f:
     requirements = f.read().splitlines()
 
-setup(name="huggan", install_requires=requirements, packages=find_packages())
+setup(
+    name="huggan",
+    install_requires=requirements,
+    packages=find_packages(),
+)
